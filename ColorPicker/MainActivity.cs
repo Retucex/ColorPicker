22c791acd19a22f26bcf0b27fbe1bb556f28cfad
Xecuter <sam.h.leclerc@gmail.com>
2016-04-22 21:51:36 -0400

    -Added final color picking logic to SV square -Added preview ImageView for color picked

--- ColorPicker/MainActivity.cs
+++ ColorPicker/MainActivity.cs
@@ -15,7 +15,11 @@ namespace ColorPicker
 	{
 		ImageView hueRing;
 		ImageView svSquare;
+		ImageView finalColor;
 		double hue = 0;
+		double saturation = 0.5;
+		double value = 0.5;
+		bool insideSVSquare = false;
 
 		protected override void OnCreate(Bundle bundle)
 		{
@@ -26,6 +30,10 @@ namespace ColorPicker
 			hueRing.Touch += HueRingTouchEvent;
 
 			svSquare = FindViewById<ImageView>(Resource.Id.imageView2);
+			svSquare.SetImageBitmap(Helper.GenerateSVSquareBitmap(Resources, 0));
+
+			finalColor = FindViewById<ImageView>(Resource.Id.imageView3);
+			finalColor.SetColorFilter(Color.HSVToColor(new[] { (float)hue, (float)saturation, (float)value }), PorterDuff.Mode.SrcAtop);
 		}
 
 		public void HueRingTouchEvent(object sender, View.TouchEventArgs eventArgs)
@@ -33,22 +41,38 @@ namespace ColorPicker
 			switch (eventArgs.Event.Action)
 			{
 				case MotionEventActions.Down:
+					if (eventArgs.Event.RawX >= 240 && eventArgs.Event.RawX <= 840 && eventArgs.Event.RawY >= 660
+						&& eventArgs.Event.RawY <= 1260)
+					{
+						insideSVSquare = true;
+					}
 					Console.WriteLine("Begins");
 					break;
 
 				case MotionEventActions.Move:
-					int[] xy = new int[2];
-					hueRing.GetLocationOnScreen(xy);
-					hue = Helper.GetAngleInDegrees(
-						hueRing.Width / 2,
-						hueRing.Height / 2 + xy[1],
-						eventArgs.Event.RawX,
-						eventArgs.Event.RawY);
-					Console.WriteLine($"Hue: {hue}");
-					svSquare.SetImageBitmap(Helper.GenerateSVSquareBitmap(Resources, hue));
+					if (insideSVSquare)
+					{
+						saturation = Helper.Clamp((eventArgs.Event.RawX - 240) / 600, 0, 1);
+						value = Helper.Clamp((eventArgs.Event.RawY - 660) / 600, 0, 1);
+					}
+					else
+					{
+						int[] xy = new int[2];
+						hueRing.GetLocationOnScreen(xy);
+						hue = Helper.GetAngleInDegrees(
+							hueRing.Width / 2,
+							hueRing.Height / 2 + xy[1],
+							eventArgs.Event.RawX,
+							eventArgs.Event.RawY);
+						svSquare.SetImageBitmap(Helper.GenerateSVSquareBitmap(Resources, hue));
+					}
+					finalColor.SetColorFilter(Color.HSVToColor(new []{(float)hue, (float)saturation, (float)value }), PorterDuff.Mode.SrcAtop);
+					//Console.WriteLine($"{eventArgs.Event.RawX}, {eventArgs.Event.RawY}");
+					//Console.WriteLine($"{saturation}, {value}");
 					break;
 
 				case MotionEventActions.Up:
+					insideSVSquare = false;
 					Console.WriteLine("Ends");
 					break;
 
--- ColorPicker/Resources/Resource.Designer.cs
+++ ColorPicker/Resources/Resource.Designer.cs
@@ -66,11 +66,14 @@ namespace ColorPicker
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050002
+			public const int imageView1 = 2131034114;
+			
 			// aapt resource value: 0x7f050001
-			public const int imageView1 = 2131034113;
+			public const int imageView2 = 2131034113;
 			
 			// aapt resource value: 0x7f050000
-			public const int imageView2 = 2131034112;
+			public const int imageView3 = 2131034112;
 			
 			static Id()
 			{
